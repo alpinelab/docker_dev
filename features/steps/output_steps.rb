9a0810b03c0aa77ec1c2d9feeae61ebfb33173ad
TJ <thibault.jouan@alpine-lab.com>
2020-09-21 08:36:41 +0000

    Refactor usage UATs

---
+++ features/steps/output_steps.rb
@@ -0,0 +1,14 @@
+usage = <<-eoh
+Usage: docker_dev [options]
+    -h, --help                       print this message
+        --postgresql                 use PostgreSQL
+eoh
+
+
+Then 'the standard output must contain exactly the usage' do
+  expect(last_command_started.output).to eq usage
+end
+
+Then 'the standard error must contain exactly the usage' do
+  expect(last_command_started.stderr).to eq usage
+end
